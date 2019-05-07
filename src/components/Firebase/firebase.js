5f8ed5305b0baca8a49995f7c913ed4644e93cb5
Andrew Chow <chowlick@gmail.com>
2019-05-07 16:30:41 -0700

    signup and signin

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import app from 'firebase/app';
+import 'firebase/auth';
 
-const Firebase = () => (
-  <div>
-    <h1>Firebase</h1>
-  </div>
-);
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+}
 
 export default Firebase;
+
+// const config = {
+//   apiKey: "AIzaSyCO_RWmCtS4iiq6JmAryC1B7NRfZkymS1A",
+//   authDomain: "authentication-template-f0be0.firebaseapp.com",
+//   databaseURL: "https://authentication-template-f0be0.firebaseio.com",
+//   projectId: "authentication-template-f0be0",
+//   storageBucket: "authentication-template-f0be0.appspot.com",
+//   messagingSenderId: "249217897473"
+// };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import Firebase, { FirebaseContext } from './components/Firebase/Index';
+
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>
+    , document.getElementById('root')
+);
 
 //https://www.robinwieruch.de/complete-firebase-authentication-react-tutorial/
 serviceWorker.unregister();
+
